17379979b07b255fbca3450526834215d8e86c1a
unknown <you@example.com>
2018-04-03 15:22:14 +0300

    about-me section

--- gulpfile.js
+++ gulpfile.js
@@ -162,3 +162,4 @@ gulp.task(
     gulp.parallel("watch", "server")
   )
 );
+
--- src/scripts/modules/module.js
+++ src/scripts/modules/module.js
@@ -1,1 +1,15 @@
 console.log('hello from module');
+
+var menu__hamburger = document.querySelector(".menu__hamburger");
+console.log(menu__hamburger);
+menu__hamburger.addEventListener("click", function() {    
+  var mobilemenu__content = document.querySelector(".full-screen__list");
+  console.log(mobilemenu__content.classList, '.full-screen__-list.classList');
+  if(menu__hamburger.classList.contains("menu__hamburger_active")) {
+    menu__hamburger.classList.remove("menu__hamburger_active");
+    mobilemenu__content.classList.remove("active");
+  } else {
+    menu__hamburger.classList.add("menu__hamburger_active");
+    mobilemenu__content.classList.add("active");
+  }
+});
